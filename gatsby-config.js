66602818f5df19ecc123d23c87c665c2c9564ffb
Christian Diomampo <chrisgen19@gmail.com>
2021-03-20 20:45:26 +0800

    Add Google Analytic

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,14 @@ module.exports = {
         icon: 'src/images/favicon.png',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-9773MDELM4', // Google Analytics / GA
+        ],
+      },
+    },
   ],
 };
